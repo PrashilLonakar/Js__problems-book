064837e9d2023484a85690c1f3439a4d618200cb
PrashilLonakar <prashil.lonakar@gmail.com>
2023-04-18 12:10:22 +0530

    padStrat and padEnd

--- String_property/concat.js
+++ String_property/concat.js
@@ -2,6 +2,8 @@ let text1 = "sea";
 let text2 = "food";
 let result = text1.concat(text2);
 
+let result3 = text1.concat(" ", text2);
+
 let result2 = text1 + text2;
 
 // The concat() method joins two or more strings.
@@ -10,5 +12,11 @@ let result2 = text1 + text2;
 
 // seafood
 
+text3 = "Hello" + " " + "World!";
+text4 = "Hello".concat(" ", "World!");
+
 console.log(result);
 console.log(result2);
+console.log(result3);
+console.log(text3);
+console.log(text4);
