c1844066a2f70ecc04cdb6e2865a36e9a2295f77
prashil-lonakar <prashil.l@volgainfotech.com>
2024-06-13 18:21:55 +0530

    reverse string in an array code without inbuild func done

--- Array/reverseStringInArrOfStr.js
+++ Array/reverseStringInArrOfStr.js
@@ -1,7 +1,13 @@
 let arrOfStr = ["my", "name", "is", "Prashil"];
 let reversearrOfStr = [];
 for (str of arrOfStr) {
-  reversearrOfStr.push(str.split("").reverse().join(""));
+  let strArr = str.split("");
+  let reverseStrArr = [];
+  for (let j = strArr.length - 1; j >= 0; j--) {
+    reverseStrArr.push(strArr[j]);
+  }
+  reversearrOfStr.push(reverseStrArr.join(""));
+  // reversearrOfStr.push(str.split("").reverse().join(""));
 }
 
 console.log("reversearrOfStr", reversearrOfStr);
