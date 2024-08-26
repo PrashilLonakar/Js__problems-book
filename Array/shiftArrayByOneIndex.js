91fc65aff9c8e21f9e0bf0daa796f56e3a375088
prashil-lonakar <prashil.l@volgainfotech.com>
2024-08-26 14:42:38 +0530

    shift array and 189.Rotate Array leetcode problem solved

---
+++ Array/shiftArrayByOneIndex.js
@@ -0,0 +1,56 @@
+let arr = [1, 2, 3, 4, 5]; //expected o/p : [ 2, 3, 4, 5, 1 ]
+
+// 1] With using new Array
+let resultArr = [];
+
+for (let i = 0; i <= arr.length - 1; i++) {
+    console.log('arr.length - 1', arr.length - 1);
+
+    if (i == arr.length - 1) {
+        resultArr[i] = arr[0];
+    } else {
+        resultArr[i] = arr[i + 1];
+    }
+}
+
+console.log(resultArr);
+
+// 2] Without using new Array but using variable
+
+let arr2 = [1, 2, 3, 4, 5]; //expected o/p : [ 2, 3, 4, 5, 1 ]
+let index0 = arr2[0];
+for (let i = 0; i <= arr2.length - 1; i++) {
+    console.log('arr2.length - 1', arr2.length - 1);
+
+    if (i == arr2.length - 1) {
+        arr2[i] = index0;
+    } else {
+        arr2[i] = arr2[i + 1];
+    }
+}
+
+console.log(arr2);
+
+//3] Without using for loop
+
+let arr3 = [1, 2, 3, 4, 5]; // Expected output: [2, 3, 4, 5, 1]
+
+// Store the first element
+let firstElement = arr3.shift();
+console.log('firstElement', firstElement);
+arr3.push(firstElement);
+console.log('arr3', arr3);
+
+//3] With using for loop changing only one line
+
+let arr4 = [1, 2, 3, 4, 5]; // Expected output: [2, 3, 4, 5, 1]
+
+for (let i = 0; i <= arr4.length - 1; i++) {
+    if (i == arr4.length - 1) {
+        // Move the first element to the end of the array
+        let firstElement = arr4.shift();
+        arr4.push(firstElement);
+    }
+}
+
+console.log('arr4', arr4);
---
+++ Leetcode/189.RotateArray/index.js
@@ -0,0 +1,122 @@
+// 189. Rotate Array
+
+// Medium
+
+// Topics
+
+// Companies
+
+// Hint
+
+// Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+// Example 1:
+
+// Input: nums = [1,2,3,4,5,6,7], k = 3
+
+// Output: [5,6,7,1,2,3,4]
+
+// Explanation:
+
+// rotate 1 steps to the right: [7,1,2,3,4,5,6]
+
+// rotate 2 steps to the right: [6,7,1,2,3,4,5]
+
+// rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+// Example 2:
+
+// Input: nums = [-1,-100,3,99], k = 2
+
+// Output: [3,99,-1,-100]
+
+// Explanation:
+
+// rotate 1 steps to the right: [99,-1,-100,3]
+
+// rotate 2 steps to the right: [3,99,-1,-100]
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+
+// -231 <= nums[i] <= 231 - 1
+
+// 0 <= k <= 105
+
+// Follow up:
+
+// Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
+
+// Could you do it in-place with O(1) extra space?
+
+// Accepted 2.4M Submissions 5.9M Acceptance Rate 41.4%
+
+//Example 1 :
+
+// Solution 1:
+//Input:
+let nums = [1, 2, 3, 4, 5, 6, 7];
+let k = 3;
+
+for (let i = 0; i <= k; i++) {
+    let firstElement = nums.shift();
+    nums.push(firstElement);
+    console.log('firstElement');
+}
+
+console.log('nums', nums);
+
+// Solution 2:
+//Input:
+
+let nums2 = [1, 2, 3, 4, 5, 6, 7];
+let index0 = nums2[0];
+for (let i = 0; i <= k; i++) {
+    index0 = nums2[0];
+    for (let j = 0; j <= nums2.length - 1; j++) {
+        if (j == nums2.length - 1) {
+            nums2[j] = index0;
+        } else {
+            nums2[j] = nums2[j + 1];
+        }
+    }
+}
+
+console.log('nums2', nums2);
+
+//Example 2:
+
+// Solution 1:
+// Input: nums = [-1,-100,3,99], k = 2
+
+// Output: [3,99,-1,-100]
+
+let nums3 = [-1, -100, 3, 99];
+let k2 = 2;
+
+for (let i = 1; i <= k2; i++) {
+    let firstElement = nums3.shift();
+    nums3.push(firstElement);
+    console.log('firstElement');
+}
+
+console.log('nums3', nums3);
+
+// Solution 2:
+//Input:
+
+let nums4 = [-1, -100, 3, 99];
+let index00 = nums4[0];
+for (let i = 1; i <= k2; i++) {
+    index00 = nums4[0];
+    for (let j = 0; j <= nums4.length - 1; j++) {
+        if (j == nums4.length - 1) {
+            nums4[j] = index00;
+        } else {
+            nums4[j] = nums4[j + 1];
+        }
+    }
+}
+
+console.log('nums4', nums4);
